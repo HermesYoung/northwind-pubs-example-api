ed6885db4343d5efac6064e4982ff14e71368054
HermesYoung <tails777tw@gmail.com>
2024-12-22 19:39:19 +0800

    Add extension for DI

---
+++ src/DatabaseContext/Extensions/DatabaseContextExtensions.cs
@@ -0,0 +1,17 @@
+﻿using DatabaseContext.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DatabaseContext.Extensions;
+
+public static class DatabaseContextExtensions
+{
+    public static IServiceCollection AddDatabaseContext(this IServiceCollection services, string connectionString)
+    {
+        services.AddDbContext<NorthwindPubsDbContext>(builder =>
+        {
+            builder.UseSqlServer(connectionString);
+        });
+        return services;
+    }
+}
